11fbcbef6f507ca5b7449c8f04d414b3e5855d38
Reapism <reapsprgm@gmail.com>
2020-04-04 00:30:29 -0400

    Add new types of generation constants, used for creating a character set specifically for this.

---
+++ RSG.Library/Constants/GenerationConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSG.Library.Constants
+{
+    public static class GenerationConstants
+    {
+        public const string NORMAL = "Normal";
+        public const string PRIMES = "Primes";
+
+    }
+}
