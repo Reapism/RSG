a0818c639f5ad1c1800a96de77eb7460953d4e78
Reapism <reapsprgm@gmail.com>
2020-04-04 00:26:40 -0400

    Add services

---
+++ RSG.Library/Services/CharacterSetService.cs
@@ -0,0 +1,46 @@
+﻿using RSG.Library.Models;
+using RSG.Library.Utilities;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+
+namespace RSG.Library.Services
+{
+    internal class CharacterSetService
+    {
+        public ConcurrentDictionary<string, CharacterSet> CharacterSets { get; set; }
+
+        public char[] GetNewCharacterList()
+        {
+            return ScrambleCharacterList();
+        }
+
+        private char[] ScrambleCharacterList()
+        {
+            var allCharacters = CleanCharacterList().ToCharArray();
+            ScrambleStringUtility.KnuthShuffle(allCharacters, RandomProvider.Rnd);
+
+            return allCharacters;
+        }
+
+        private string CleanCharacterList()
+        {
+            var characterList = GetCharacterList();
+            return characterList.Distinct().ToString();
+        }
+
+        private string GetCharacterList()
+        {
+            var strBuilder = new StringBuilder();
+            var enabledCharacterSets = CharacterSets.Values.Where(set => set.Enabled);
+
+            foreach (var set in enabledCharacterSets)
+            {
+                strBuilder.Append(set.Characters);
+            }
+
+            return strBuilder.ToString();
+        }
+
+    }
+}
---
+++ RSG.Library/Services/RandomProvider.cs
@@ -0,0 +1,31 @@
+﻿using RSG.Library.Constants;
+
+namespace RSG.Library.Services
+{
+    public static class RandomProvider
+    {
+        private static System.Random PsuedoRandom;
+        private static System.Random ReapRandom;
+
+        public static string SelectedRandomizationType { get; set; }
+
+        static RandomProvider()
+        {
+            PsuedoRandom = new System.Random();
+            ReapRandom = new System.Random(PsuedoRandom.Next(int.MaxValue));
+        }
+
+        public static System.Random Rnd
+        {
+            get
+            {
+                return SelectedRandomizationType switch
+                {
+                    RandomizationConstants.PSEUDORANDOM => PsuedoRandom,
+                    RandomizationConstants.REAP_RANDOM => ReapRandom,
+                    _ => PsuedoRandom,
+                };
+            }
+        }
+    }
+}
