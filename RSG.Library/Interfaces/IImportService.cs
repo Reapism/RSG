41eab0d85f120a417ad0e9262d1d7a0aad250048
Reapism <reapsprgm@gmail.com>
2020-04-04 00:28:38 -0400

    Add new interfaces

---
+++ RSG.Library/Interfaces/IImportService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RSG.Library.Interfaces
+{
+    internal interface IImportService
+    {
+        IEnumerable<string> Import(string path, Encoding info);
+    }
+}
---
+++ RSG.Library/Interfaces/IUpdateService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSG.Library.Interfaces
+{
+    internal interface IUpdateService
+    {
+        public string Version { get; set; }
+        public string Source { get; set; } 
+
+        public bool IsNewVersionAvailable();
+    }
+}
