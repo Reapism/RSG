d82b48aa80584794f8689d9f834e2a802b61e821
Reapism <reapsprgm@gmail.com>
2020-04-04 00:29:55 -0400

    Implement a KnuthShuffle for an optimal mathematical scramble of an array of things.

---
+++ RSG.Library/Utilities/ScrambleStringUtility.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace RSG.Library.Utilities
+{
+    public static class ScrambleStringUtility
+    {
+        public static void KnuthShuffle<T>(T[] array, Random random)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                int j = random.Next(i, array.Length); // Don't select from the entire array on subsequent loops
+                T temp = array[i]; array[i] = array[j]; array[j] = temp;
+            }
+        }
+    }
+}
