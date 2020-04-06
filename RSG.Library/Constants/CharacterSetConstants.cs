d02baeb7c9337fdc9888101e6e31758bdda869df
Reapism <reapsprgm@gmail.com>
2020-04-05 23:02:52 -0400

    Constants changes

---
+++ RSG.Library/Constants/CharacterSetConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace RSG.Library.Constants
+{
+    public static class CharacterSetConstants
+    {
+        public const string LOWERCASE = "Lowercase";
+        public const string UPPERCASE = "Uppercase";
+        public const string NUMBERS = "Numbers";
+        public const string PUNCTUATION = "Punctuation";
+        public const string SPACE = "Space";
+        public const string SYMBOLS = "Symbols";
+
+        public const string LOWERCASE_SET = "abcdefghijklmnopqrstuvwxyz";
+        public const string UPPERCASE_SET = "ABCDEFGHIJKLMNAOPQRSTUVWXYZ";
+        public const string NUMBERS_SET = "0123456789";
+        public const string PUNCTUATION_SET = "!,.?";
+        public const string SPACE_SET = " ";
+        public const string SYMBOLS_SET = " !@#$%^&*()-=_+,./;\'\\<>?:\"[]{}|`~";
+    }
+}
--- RSG.Library/Constants/GenerationConstants.cs
+++ RSG.Library/Constants/GenerationConstants.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RSG.Library.Constants
+﻿namespace RSG.Library.Constants
 {
     public static class GenerationConstants
     {
