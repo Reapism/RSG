865db59e86b06f42a488cb99dfba50552fc58e60
Reapism <reapsprgm@gmail.com>
2020-04-05 23:03:03 -0400

    Interface changes

---
+++ RSG.Library/Interfaces/ICharacterSet.cs
@@ -0,0 +1,10 @@
+﻿using RSG.Library.Models;
+using System.Collections.Generic;
+
+namespace RSG.Library.Interfaces
+{
+    public interface ICharacterSet
+    {
+        IDictionary<string, CharacterSet> CharacterSets { get; set; }
+    }
+}
--- RSG.Library/Interfaces/IUpdateService.cs
+++ RSG.Library/Interfaces/IUpdateService.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace RSG.Library.Interfaces
+﻿namespace RSG.Library.Interfaces
 {
     internal interface IUpdateService
     {
         public string Version { get; set; }
-        public string Source { get; set; } 
+        public string Source { get; set; }
 
         public bool IsNewVersionAvailable();
     }
