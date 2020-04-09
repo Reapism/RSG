f93ebc689fd279cfa47ad8b78fe9390bcec4cb1c
Reapism <reapsprgm@gmail.com>
2020-04-08 22:18:41 -0400

    Adding the windows

---
+++ RSG.View/Windows/RsgWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace RSG.View.Windows
+{
+    /// <summary>
+    /// Interaction logic for RsgWindow.xaml
+    /// </summary>
+    public partial class RsgWindow : Window
+    {
+        public RsgWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- RSG.View/Windows/SearchWindow.xaml.cs
+++ RSG.View/Windows/SearchWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace RSG.View.Windows
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for SearchWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class SearchWindow : Window
     {
-        public MainWindow()
+        public SearchWindow()
         {
             InitializeComponent();
         }
