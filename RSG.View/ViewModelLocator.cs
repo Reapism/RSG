96f8a41084183e707b8c083ed90f53010f6448ad
Reapism <reapsprgm@gmail.com>
2020-07-06 19:05:21 -0400

    ViewModelLocator - POC. Will remove and replace with prism

---
+++ RSG.View/ViewModelLocator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows;
+
+namespace RSG.View
+{
+    public static class ViewModelLocator
+    {
+        public static bool GetAutoWireViewModel(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(AutoWireViewModelProperty);
+        }
+
+        public static void SetAutoWireViewModel(DependencyObject obj, bool value)
+        {
+            obj.SetValue(AutoWireViewModelProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AutoWireViewModelProperty =
+            DependencyProperty.RegisterAttached("MyProperty", typeof(bool), typeof(ViewModelLocator), new PropertyMetadata(false, AutoWireViewModelChanged));
+
+        private static void AutoWireViewModelChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+                return;
+
+            var viewTypeName = d.GetType().FullName;
+            var viewModelTypeName = $"{viewTypeName}Model";
+            var viewModelType = Type.GetType(viewModelTypeName);
+
+            var viewModel = Activator.CreateInstance(viewModelType);
+            ((FrameworkElement)d).DataContext = viewModel;
+
+        }
+    }
+}
