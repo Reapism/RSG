e4979acc6fb565dcffe05891f5d96e6815c53e52
Reapism <reapsprgm@gmail.com>
2020-07-06 23:29:12 -0400

    Updating the dictionary configuration

--- RSG.Core/Configuration/DictionaryConfiguration.cs
+++ RSG.Core/Configuration/DictionaryConfiguration.cs
@@ -1,5 +1,5 @@
-﻿using RSG.Core.Interfaces;
-using RSG.Core.Interfaces.Configuration;
+﻿using RSG.Core.Interfaces.Configuration;
+using RSG.Core.Models;
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
@@ -7,56 +7,80 @@ using System.Threading;
 
 namespace RSG.Core.Configuration
 {
-    public class DictionaryConfiguration : IDictionaryConfiguration, ILoadConfiguration<DictionaryConfiguration>
+    public class DictionaryConfiguration : IDictionaryConfiguration
     {
         private bool isFullyInitialized;
 
         public DictionaryConfiguration()
         {
-            if (DoesConfigurationExist())
-            {
-                // read into dictionaries, otherwise set to empty.
-            }
         }
-
-        public T Load<T>(string fileName)
-        {
-            return default(T);
-        }
-
-        public IEnumerable<IRsgDictionary> Dictionaries { get; set; }
 
         /// <summary>
-        /// Gets or sets source of the serialized configuration.
-        /// <para>Must be a local file.</para>
+        /// Gets or sets a <see cref="IList{T}"/> of <see cref="RsgDictionary"/>(s).
+        /// </summary>
+        public IList<RsgDictionary> Dictionaries { get; set; }
+
+        /// <summary>
+        /// Gets or sets the source of the configuration file.
+        /// <para>Must be a local source.</para>
         /// </summary>
         [JsonIgnore]
         public string Source { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to use spaces in generating words or not.
+        /// </summary>
         public bool UseSpace { get; set; }
 
-        public bool CapitalizeEachWord { get; set; }
-
-        public char AliterationCharacter { get; set; }
-
-        public double AliterationFrequency { get; set; }
-
+        /// <summary>
+        /// Gets or sets a value indicating whether to use noise in generating words or not.
+        /// </summary>
         public bool UseNoise { get; set; }
 
-        public double NoiseFrequency { get; set; }
-
-        public int MaximumThreadCount { get; set; }
-
-        public ThreadPriority Priority { get; set; }
+        /// <summary>
+        /// Gets or sets a value indicating whether to use aliteration in generating words or not.
+        /// </summary>
+        public bool UseAliteration { get; set; }
 
         /// <summary>
-        /// -1 if <see cref="Words"/> should automatically determine.
+        /// Gets or sets a value indicating whether to capitailize each generated word or not.
         /// </summary>
-        public int PartitionSize { get; set; }
+        public bool CapitalizeEachWord { get; set; }
 
-        private bool DoesConfigurationExist()
-        {
-            return false;
-        }
+        /// <summary>
+        /// Gets or sets a value indicating the aliteration character used when generating words.
+        /// <para>"\u0000" for none.</para>
+        /// </summary>
+        public char AliterationCharacter { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the frequency in using aliteration words.
+        /// </summary>
+        public double AliterationFrequency { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the frequency in generating words with noise.
+        /// </summary>
+        public double NoiseFrequency { get; set; }
+
+        /// <summary>
+        /// Gets or sets a range value indicating the number of noise to be generated per word.
+        /// </summary>
+        public Range NoisePerWordRange { get; set; }
+
+        /// <summary>
+        /// Gets or sets a range value indicating the number of aliterations to be generated.
+        /// </summary>
+        public Range AliterationRange { get; set; }
+
+        /// <summary>
+        /// Gets or sets a maximum value indicating the number of threads the service can use for the generation procedure.
+        /// </summary>
+        public int MaximumThreadCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets a <see cref="ThreadPriority"/> value indicating priority of the generation.
+        /// </summary>
+        public ThreadPriority Priority { get; set; }
     }
 }
--- RSG.Core/Interfaces/Configuration/IDictionaryConfiguration.cs
+++ RSG.Core/Interfaces/Configuration/IDictionaryConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using RSG.Core.Models;
+using System;
 using System.Collections.Generic;
 using System.Threading;
 
@@ -6,24 +7,71 @@ namespace RSG.Core.Interfaces.Configuration
 {
     public interface IDictionaryConfiguration
     {
-        IEnumerable<RsgDictionary> Dictionaries { get; set; }
+        /// <summary>
+        /// Gets or sets a <see cref="IList{T}"/> of <see cref="RsgDictionary"/>(s).
+        /// </summary>
+        IList<RsgDictionary> Dictionaries { get; set; }
 
+        /// <summary>
+        /// Gets or sets the source of the configuration file.
+        /// <para>Must be a local source.</para>
+        /// </summary>
         string Source { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to use spaces in generating words or not.
+        /// </summary>
         bool UseSpace { get; set; }
 
-        bool CapitalizeEachWord { get; set; }
-
+        /// <summary>
+        /// Gets or sets a value indicating whether to use noise in generating words or not.
+        /// </summary>
         bool UseNoise { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to use aliteration in generating words or not.
+        /// </summary>
+        bool UseAliteration { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to capitailize each generated word or not.
+        /// </summary>
+        bool CapitalizeEachWord { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating the aliteration character used when generating words.
+        /// <para>Typically, "\u0000" for none.</para>
+        /// </summary>
         char AliterationCharacter { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating the frequency in using aliteration words.
+        /// </summary>
         double AliterationFrequency { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating the frequency in noise generating words.
+        /// </summary>
         double NoiseFrequency { get; set; }
 
+        /// <summary>
+        /// Gets or sets a range value indicating the number of noise to be generated per word.
+        /// </summary>
+        Range NoisePerWordRange { get; set; }
+
+        /// <summary>
+        /// Gets or sets a range value indicating the number of aliterations to be generated.
+        /// </summary>
+        Range AliterationRange { get; set; }
+
+        /// <summary>
+        /// Gets or sets a maximum value indicating the number of threads the service can use for the generation procedure.
+        /// </summary>
         int MaximumThreadCount { get; set; }
 
+        /// <summary>
+        /// Gets or sets a <see cref="ThreadPriority"/> value indicating priority of the generation.
+        /// </summary>
         ThreadPriority Priority { get; set; }
     }
 }
