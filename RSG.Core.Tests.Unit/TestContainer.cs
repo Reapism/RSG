550d6fcbdbfc1a55634561e36c658eae88a5e2a6
Reapism <reapsprgm@gmail.com>
2020-07-15 21:37:42 -0400

    Add test container to demonstrate the insanity of creating a RandomWordGenerator instance manually.

---
+++ RSG.Core.Tests.Unit/TestContainer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using RSG.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RSG.Core.Tests.Unit
+{
+    public class TestContainer
+    {
+        public IServiceProvider Provider { get; }
+        public IServiceCollection Services { get; set; }
+
+        public TestContainer()
+        {
+            
+            Services = new ServiceCollection();
+            Services.AddRsgCore();
+            Provider = Services.BuildServiceProvider();
+        }
+    }
+}
