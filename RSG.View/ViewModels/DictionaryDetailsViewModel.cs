a426af0b4f8be119af669caaef2860709626c8f7
Reapism <reapsprgm@gmail.com>
2020-07-22 00:18:19 -0400

    view changes

--- RSG.View/ViewModels/DictionaryDetailsViewModel.cs
+++ RSG.View/ViewModels/DictionaryDetailsViewModel.cs
@@ -1,5 +1,12 @@
-﻿using RSG.Core.Interfaces;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Prism.Commands;
+using RSG.Core.Interfaces;
+using RSG.Core.Utilities;
+using System;
 using System.ComponentModel;
+using System.Numerics;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace RSG.View.ViewModels
 {
@@ -7,7 +14,36 @@ namespace RSG.View.ViewModels
     {
         public DictionaryDetailsViewModel()
         {
+            var generator = App.Container.Provider.GetService<RandomWordGenerator>();
+            generator.GenerateRandomWordsResultCompleted += Generator_GenerateRandomWordsResultCompleted;
+            generator.GenerateRandomWordsResultProgressChanged += Generator_GenerateRandomWordsResultProgressChanged;
+            RunCommand = new DelegateCommand(OnRunAsync, CanRun);
+        }
 
+        private bool CanRun()
+        {
+            return true;
+        }
+
+        private void Generator_GenerateRandomWordsResultProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+
+        }
+
+        private void Generator_GenerateRandomWordsResultCompleted(object sender, GenerateRandomWordsResultEventArgs e)
+        {
+            if (!e.Cancelled && e.Error != null && e.Result != null)
+            {
+                Result = e.Result;
+            }
+        }
+
+        public DelegateCommand RunCommand { get; private set; }
+
+        public async void OnRunAsync()
+        {
+            var generator = App.Container.Provider.GetService<RandomWordGenerator>();
+            await generator.GenerateRandomWordsResult(BigInteger.Parse("100000"));
         }
 
         public IDictionaryResult Result { get; set; }
