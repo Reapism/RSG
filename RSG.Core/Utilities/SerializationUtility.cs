76c6a40052409b26cce5ff427e230e84bb73f563
Reapism <reapsprgm@gmail.com>
2020-07-22 00:14:26 -0400

    Huge update to Serialization utiltiy to support synchronous serialization and deserialization of many different forms for extreme convience.

--- RSG.Core/Utilities/SerializationUtility.cs
+++ RSG.Core/Utilities/SerializationUtility.cs
@@ -1,5 +1,8 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace RSG.Core.Utilities
@@ -7,7 +10,7 @@ namespace RSG.Core.Utilities
     /// <summary>
     /// A utility to serialize and deserialize JSON
     /// objects asynchronously.
-    /// <para>Use [JsonIgnore] to ignore properties or members from serialization.</para>
+    /// <para>Use [<see cref="JsonIgnoreAttribute"/>] to ignore properties or members from serialization.</para>
     /// </summary>
     public static class SerializationUtility
     {
@@ -50,18 +53,106 @@ namespace RSG.Core.Utilities
         }
 
         /// <summary>
+        /// Deserialize a <typeparamref name="T"/> its value
+        /// from a <see cref="ReadOnlySpan{Byte}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize.</typeparam>
+        /// <param name="utf8Json">Parses the UTF-8 encoded JSON into a <typeparamref name="T"/> instance.</param>
+        /// <param name="options">Optional serializer options.</param>
+        /// <returns>An instance of <typeparamref name="T"/>.</returns>
+        /// <exception cref="JsonException">An error occuring with the deserialization.</exception>
+        public static T DeserializeJson<T>(ReadOnlySpan<byte> utf8Json, JsonSerializerOptions options = null)
+        {
+            var obj = JsonSerializer.Deserialize<T>(utf8Json, options);
+            return obj;
+        }
+
+        /// <summary>
+        /// Deserialize a <typeparamref name="T"/> its value
+        /// from a <see cref="Stream"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize.</typeparam>
+        /// <param name="stream">A <see cref="Stream"/> containing the serialized utf8 encoded json file.</param>
+        /// <param name="options">Optional serializer options.</param>
+        /// <returns>An instance of <typeparamref name="T"/>.</returns>
+        /// <exception cref="JsonException">An error occuring with the deserialization.</exception>
+        public static T DeserializeJson<T>(Stream stream, JsonSerializerOptions options = null)
+        {
+            var memoryStream = new MemoryStream();
+            stream.CopyTo(memoryStream);
+
+            var bytes = memoryStream.ToArray();
+            var readOnlySpan = new ReadOnlySpan<byte>(bytes, 0, bytes.Length);
+            return DeserializeJson<T>(readOnlySpan, options);
+        }
+
+        /// <summary>
+        /// Deserialize a <typeparamref name="T"/> its value
+        /// from a physical file path.
+        /// </summary>
+        /// <typeparam name="T">The type to deserialize.</typeparam>
+        /// <param name="inputPath">The path to the serialized file.</param>
+        /// <param name="options">Optional serializer options.</param>
+        /// <returns>An instance of <typeparamref name="T"/>.</returns>
+        /// <exception cref="FileNotFoundException">Thrown if the file is not found.</exception>
+        /// <exception cref="JsonException">An error occuring with the deserialization.</exception>
+        public static T DeserializeJson<T>(string inputPath, JsonSerializerOptions options = null)
+        {
+            if (!IOUtility.DoesFileExist(inputPath))
+            {
+                throw new FileNotFoundException($"Can't find the find at {inputPath}.");
+            }
+
+            var bytes = File.ReadAllBytes(inputPath);
+            var readOnlySpan = new ReadOnlySpan<byte>(bytes, 0, bytes.Length);
+            var obj = JsonSerializer.Deserialize<T>(readOnlySpan, options);
+
+            return obj;
+        }
+
+        /// <summary>
         /// Serialize a <typeparamref name="T"/> asynchronously to a specific file in
         /// Utf-8 format.
         /// </summary>
-        /// <typeparam name="T">The type to deserialize.</typeparam>
-        /// <param name="type">The type to serialize.</param>
+        /// <typeparam name="T">The type to serialize.</typeparam>
+        /// <param name="value">The value to serialize.</param>
         /// <param name="outputPath">The output path to the new serialized file.</param>
         /// <param name="options">Optional serializer options.</param>
         /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
-        public static async Task SerializeJsonAsync<T>(T type, string outputPath, JsonSerializerOptions options = null)
+        public static async Task SerializeJsonAsync<T>(T value, string outputPath, JsonSerializerOptions options = null)
         {
             var fileStream = new FileStream(outputPath, FileMode.OpenOrCreate, FileAccess.Write);
-            await JsonSerializer.SerializeAsync(fileStream, type, options);
+            await JsonSerializer.SerializeAsync(fileStream, value, options);
+        }
+
+        /// <summary>
+        /// Serialize a <typeparamref name="T"/> synchronously to a specific file in
+        /// Utf-8 format.
+        /// </summary>
+        /// <typeparam name="T">The type to serialize.</typeparam>
+        /// <param name="value">The value to serialize.</param>
+        /// <param name="outputPath">The output path to the new serialized file.</param>
+        /// <param name="options">Optional serializer options.</param>
+        public static void SerializeJson<T>(T value, string outputPath, JsonSerializerOptions options = null)
+        {
+            var jsonString = JsonSerializer.Serialize(value, options);
+            File.WriteAllText(outputPath, jsonString);
+        }
+
+        /// <summary>
+        /// Serialize a <typeparamref name="T"/> synchronously into a <see cref="ReadOnlySpan{byte}"/>.
+        /// </summary>
+        /// <typeparam name="T">The type to serialize.</typeparam>
+        /// <param name="value">The value to serialize.</param>
+        /// <param name="options">Optional serializer options.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        public static ReadOnlySpan<byte> SerializeJson<T>(T value, JsonSerializerOptions options = null)
+        {
+            var jsonString = JsonSerializer.Serialize(value, options);
+            var bytes = Encoding.UTF8.GetBytes(jsonString);
+            var readOnlySpan = new ReadOnlySpan<byte>(bytes, 0, bytes.Length);
+
+            return readOnlySpan;
         }
     }
 }
