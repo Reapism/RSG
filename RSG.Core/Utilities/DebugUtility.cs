243b5a9084b2477212245f449ce4e3e446cd7c7c
Reapism <reapsprgm@gmail.com>
2020-08-02 15:03:13 -0400

    Pretty up debug utility. Will become a simple log utility later or simply using one of the frameworks.

--- RSG.Core/Utilities/DebugUtility.cs
+++ RSG.Core/Utilities/DebugUtility.cs
@@ -5,21 +5,23 @@ namespace RSG.Core.Utilities
 {
     public static class DebugUtility
     {
-        public static Dictionary<int, (string, string)> debugKvp;
-        private static int index = 0;
+        public static Dictionary<int, (string, string)> DebugByIndex { get; set; }
+
+        private static int Index = 0;
+
         private static object locked = new object();
 
         static DebugUtility()
         {
-            debugKvp = new Dictionary<int, (string, string)>();
+            DebugByIndex = new Dictionary<int, (string, string)>();
         }
 
         public static void Write((string, string) value)
         {
             lock (locked)
             {
-                Interlocked.Increment(ref index);
-                debugKvp.Add(index, value);
+                Interlocked.Increment(ref Index);
+                DebugByIndex.Add(Index, value);
             }
         }
     }
