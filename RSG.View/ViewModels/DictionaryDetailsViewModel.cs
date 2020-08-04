2e1ac8178425decc5a8e8546ab0dc2b308c2c020
Reapism <reapsprgm@gmail.com>
2020-08-03 23:01:01 -0400

    use mvvm light

--- RSG.View/ViewModels/DictionaryDetailsViewModel.cs
+++ RSG.View/ViewModels/DictionaryDetailsViewModel.cs
@@ -1,32 +1,34 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using Prism.Commands;
 using RSG.Core.Interfaces;
 using RSG.Core.Utilities;
 using System.ComponentModel;
 using System.Numerics;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.CommandWpf;
+using System;
+using GalaSoft.MvvmLight.Ioc;
+using System.Windows;
 
 namespace RSG.View.ViewModels
 {
-    public class DictionaryDetailsViewModel : INotifyPropertyChanged
+    public class DictionaryDetailsViewModel : ViewModelBase
     {
         public DictionaryDetailsViewModel()
         {
-            var generator = App.Container.Provider.GetService<RandomWordGenerator>();
-            generator.GenerateRandomWordsResultCompleted += Generator_GenerateRandomWordsResultCompleted;
-            generator.GenerateRandomWordsResultProgressChanged += Generator_GenerateRandomWordsResultProgressChanged;
-            RunCommand = new DelegateCommand(OnRunAsync, CanRun);
+            RunCommand = new RelayCommand(OnRunAsync, canExecute: CanRun());
         }
 
-        private bool CanRun()
+        private Func<bool> CanRun()
         {
-            return true;
+            return () => true;
         }
 
         private void Generator_GenerateRandomWordsResultProgressChanged(object sender, ProgressChangedEventArgs e)
         {
-
+            if (e.ProgressPercentage > 0)
+                MessageBox.Show(e.ProgressPercentage.ToString());
         }
-
+        // TODO - move events to code behind. and figure out how to properly use events with MVVM
         private void Generator_GenerateRandomWordsResultCompleted(object sender, GenerateRandomWordsResultEventArgs e)
         {
             if (!e.Cancelled && e.Error != null && e.Result != null)
@@ -35,16 +37,22 @@ namespace RSG.View.ViewModels
             }
         }
 
-        public DelegateCommand RunCommand { get; private set; }
-
         public async void OnRunAsync()
         {
             var generator = App.Container.Provider.GetService<RandomWordGenerator>();
-            await generator.GenerateRandomWordsResult(BigInteger.Parse("100000"));
+            generator.GenerateRandomWordsResultCompleted += Generator_GenerateRandomWordsResultCompleted;
+            generator.GenerateRandomWordsResultProgressChanged += Generator_GenerateRandomWordsResultProgressChanged;
+            await generator.GenerateRandomWordsResult(BigInteger.Parse("1000000"));
+
+            //foreach (var c in DebugUtility.debugKvp)
+            //{
+            //    MessageBox.Show($"{c.Key} - {c.Value}");
+            //}
+
         }
+        public RelayCommand RunCommand { get; set; }
 
         public IDictionaryResult Result { get; set; }
 
-        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
