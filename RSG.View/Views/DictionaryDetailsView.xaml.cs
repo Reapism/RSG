62c6591cd22aa87ccd715a792cb533d27da9d70b
Reapism <reapsprgm@gmail.com>
2020-09-23 22:50:30 -0400

    Remove view model locator things, will add back once core functionality is in

--- RSG.View/Views/DictionaryDetailsView.xaml.cs
+++ RSG.View/Views/DictionaryDetailsView.xaml.cs
@@ -1,4 +1,10 @@
-﻿using System.Windows.Controls;
+﻿using RSG.Core.Utilities;
+using System;
+using System.Globalization;
+using System.Numerics;
+using System.Reflection;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace RSG.View.Views
 {
@@ -7,9 +13,43 @@ namespace RSG.View.Views
     /// </summary>
     public partial class DictionaryDetailsView : UserControl
     {
+        private readonly RandomWordGenerator randomWordGenerator;
+
         public DictionaryDetailsView()
         {
             InitializeComponent();
+
+            randomWordGenerator = IocContainer.Container.GetInstance<RandomWordGenerator>();
+
+            SubscribeEvents();
+        }
+
+        private void SubscribeEvents()
+        {
+            randomWordGenerator.GenerateCompleted += RandomWordGenerator_GenerateRandomWordsResultCompleted;
+            randomWordGenerator.GenerateChanged += RandomWordGenerator_GenerateRandomWordsResultProgressChanged;
+            
+        }
+
+        private void RandomWordGenerator_GenerateRandomWordsResultProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            MessageBox.Show(e.ProgressPercentage.ToString());
+            this.Progress.Value = e.ProgressPercentage;
+        }
+
+        private void RandomWordGenerator_GenerateRandomWordsResultCompleted(object sender, DictionaryEventArgs e)
+        {
+            var result = e.Result;
+
+            var nonGeneratedWords = result.Words.GetNumberOfOccurencesFor("aardvark");
+            var generatedWords = (int)result.Words.Count - nonGeneratedWords;
+            var perc = (double)generatedWords / (int)result.Words.Count;
+            MessageBox.Show($"{nonGeneratedWords} out of {result.Words.Count} were not generated successfully. {perc.ToString("P",CultureInfo.InvariantCulture)} successfully generated!");
+        }
+
+        private async void RunCommand_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            await randomWordGenerator.Generate(BigInteger.Parse("100004"));
         }
     }
 }
