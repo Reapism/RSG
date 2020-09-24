fc3de0d5fbe43c914fc16e5acfeac23784c82220
Reapism <reapsprgm@gmail.com>
2020-09-23 22:48:56 -0400

    Rename some events and delegates for easier readability

--- RSG.Core/Utilities/DictionaryEventArgs.cs
+++ RSG.Core/Utilities/DictionaryEventArgs.cs
@@ -10,31 +10,28 @@ namespace RSG.Core.Utilities
     /// </summary>
     /// <param name="sender">The object who fired the event.</param>
     /// <param name="e">The <see cref="ProgressChangedEventArgs"/>.</param>
-    public delegate void GenerateRandomWordsResultProgressChangedEventHandler(object sender, ProgressChangedEventArgs e);
+    public delegate void ProgressChanged(object sender, ProgressChangedEventArgs e);
 
     /// <summary>
     /// Event handler for when the <see cref="RandomWordGenerator.Generate(System.Numerics.BigInteger)"/>
     /// Completed event is fired.
     /// </summary>
     /// <param name="sender">The object who fired the event.</param>
-    /// <param name="e">The <see cref="GenerateRandomWordsResultEventArgs"/> args.</param>
-    public delegate void GenerateRandomWordsResultCompletedEventHandler(object sender, GenerateRandomWordsResultEventArgs e);
+    /// <param name="e">The <see cref="DictionaryEventArgs"/> args.</param>
+    public delegate void Completed(object sender, DictionaryEventArgs e);
 
-    /// <summary>
-    /// 
-    /// </summary>
-    public class GenerateRandomWordsResultEventArgs : AsyncCompletedEventArgs
+    public class DictionaryEventArgs : AsyncCompletedEventArgs
     {
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="GenerateRandomWordsResultEventArgs"/> class with a
+        /// Initializes a new instance of the <see cref="DictionaryEventArgs"/> class with a
         /// <see cref="IDictionaryResult"/> instance.
         /// </summary>
         /// <param name="e"></param>
         /// <param name="isCancelled"></param>
         /// <param name="errorState"></param>
         /// <param name="result"></param>
-        public GenerateRandomWordsResultEventArgs(Exception e, bool isCancelled, object errorState, IDictionaryResult result)
+        public DictionaryEventArgs(Exception e, bool isCancelled, object errorState, IDictionaryResult result)
             : base(e, isCancelled, errorState)
         {
             Result = result;
