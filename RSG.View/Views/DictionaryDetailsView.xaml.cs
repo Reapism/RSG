46f3593d834a74dc82664e1937e9e8eba47f5fdf
Reapism <reapsprgm@gmail.com>
2020-09-25 21:52:49 -0400

    enable and disable button on click -> completed generate event

--- RSG.View/Views/DictionaryDetailsView.xaml.cs
+++ RSG.View/Views/DictionaryDetailsView.xaml.cs
@@ -33,7 +33,6 @@ namespace RSG.View.Views
 
         private void RandomWordGenerator_GenerateRandomWordsResultProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
-            MessageBox.Show(e.ProgressPercentage.ToString());
             this.Progress.Value = e.ProgressPercentage;
         }
 
@@ -44,11 +43,13 @@ namespace RSG.View.Views
             var nonGeneratedWords = result.Words.GetNumberOfOccurencesFor("aardvark");
             var generatedWords = (int)result.Words.Count - nonGeneratedWords;
             var perc = (double)generatedWords / (int)result.Words.Count;
+            RunCommand.IsEnabled = true;
             MessageBox.Show($"{nonGeneratedWords} out of {result.Words.Count} were not generated successfully. {perc.ToString("P",CultureInfo.InvariantCulture)} successfully generated!");
         }
 
         private async void RunCommand_Click(object sender, System.Windows.RoutedEventArgs e)
         {
+            RunCommand.IsEnabled = false;
             await randomWordGenerator.Generate(BigInteger.Parse("100004"));
         }
     }
