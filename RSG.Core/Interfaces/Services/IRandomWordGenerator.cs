6a17d27ab742caa5b263cbbb358f029023c4f37f
Reapism <reapsprgm@gmail.com>
2020-10-06 00:10:52 -0400

    Add events to IRandomWordGenerator

--- RSG.Core/Interfaces/Services/IRandomWordGenerator.cs
+++ RSG.Core/Interfaces/Services/IRandomWordGenerator.cs
@@ -1,10 +1,14 @@
-﻿using System.Numerics;
+﻿using RSG.Core.Utilities;
+using System.Numerics;
 using System.Threading.Tasks;
 
 namespace RSG.Core.Interfaces.Services
 {
     public interface IRandomWordGenerator
     {
+        event Completed GenerateCompleted;
+        event ProgressChanged GenerateChanged;
+
         Task GenerateAsync(BigInteger iterations);
     }
 }
