11f0178a481dd8e7f5d5365d674e72a580318f4a
Reapism <reapsprgm@gmail.com>
2020-10-07 01:04:01 -0400

    MvvM implementation of DictionaryEditView - Commands binded to Generate - IoC properly instantiating oibjects - remove need for parameterized RelayCommand

--- RSG.View/ViewModels/DictionaryEditViewModel.cs
+++ RSG.View/ViewModels/DictionaryEditViewModel.cs
@@ -3,31 +3,64 @@ using GalaSoft.MvvmLight.Command;
 using RSG.Core.Enums;
 using RSG.Core.Extensions;
 using RSG.Core.Interfaces;
+using RSG.Core.Interfaces.Services;
 using RSG.Core.Models;
 using RSG.Core.Utilities;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 
 namespace RSG.View.ViewModels
 {
     public class DictionaryEditViewModel : ViewModelBase
     {
-        public DictionaryEditViewModel()
+        private readonly IRandomWordGenerator randomWordGenerator;
+        private int currentProgress;
+        private string iterations;
+
+        public DictionaryEditViewModel(IRandomWordGenerator randomWordGenerator)
         {
-            ViewLog = new RelayCommand(RunViewLog, CanExecuteViewLog);
-            RandomizeSettings = new RelayCommand(RunRandomizeSettings, CanExecuteRandomizeSettings);
-            Generate = new RelayCommand<RandomWordGenerator>(RunGenerate, CanExecuteGenerate);
+            ViewLogCommand = new RelayCommand(RunViewLog, CanExecuteViewLog);
+            RandomizeSettingsCommand = new RelayCommand(RunRandomizeSettings, CanExecuteRandomizeSettings);
+            GenerateCommand = new RelayCommand(RunGenerate, CanExecuteGenerate);
+            this.randomWordGenerator = randomWordGenerator;
+            this.randomWordGenerator.GenerateChanged += RandomWordGenerator_GenerateChanged;
+            this.randomWordGenerator.GenerateCompleted += RandomWordGenerator_GenerateCompleted;
         }
 
-        private async void RunGenerate(RandomWordGenerator generator)
+        private async void RandomWordGenerator_GenerateCompleted(object sender, DictionaryEventArgs e)
         {
-            await generator.GenerateAsync(Iterations.ToBigInteger());
+            if (e.Cancelled is true || (!(e.Error is null)))
+            {
+                return;
+            }
+
+            if (!(e.Result is null))
+            {
+                var nonGeneratedWords = e.Result.Words.GetNumberOfOccurencesFor("aardvark");
+                var generatedWords = (int)e.Result.Words.Count - nonGeneratedWords;
+                var perc = (double)generatedWords / (int)e.Result.Words.Count;
+                var str = ($"{nonGeneratedWords} out of {e.Result.Words.Count} were not generated successfully. {perc.ToString("P", CultureInfo.InvariantCulture)} successfully generated!");
+                var c = true;
+                GC.Collect();
+            }
+
         }
 
-        private bool CanExecuteGenerate(RandomWordGenerator generator)
+        private async void RandomWordGenerator_GenerateChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
         {
-            return (!(generator is null));
+            CurrentProgress = e.ProgressPercentage;
+        }
+
+        private async void RunGenerate()
+        {
+            await randomWordGenerator.GenerateAsync(Iterations.ToBigInteger());
+        }
+
+        private bool CanExecuteGenerate()
+        {
+            return (!(randomWordGenerator is null) && (!(Iterations is null)));
         }
 
         private void RunRandomizeSettings()
@@ -50,7 +83,6 @@ namespace RSG.View.ViewModels
             return true;
         }
 
-        public string Iterations { get; set; }
         public RsgDictionary SelectedDictionary { get; set; }
         public ObservableCollection<IRsgDictionary> Dictionaries { get; set; }
         public RandomizationType SelectedRandomizationType { get; set; }
@@ -63,9 +95,28 @@ namespace RSG.View.ViewModels
         public bool UseNoise { get; set; }
         public bool UseMaxThreads { get; set; }
 
-        public RelayCommand ViewLog { get; set; }
-        public RelayCommand RandomizeSettings { get; set; }
-        public RelayCommand<RandomWordGenerator> Generate { get; set; }
+        public string Iterations
+        {
+            get => iterations;
+            set
+            {
+                Set(nameof(Iterations), ref iterations, value);
+                GenerateCommand.RaiseCanExecuteChanged();
+            }
+        }
+
+        public int CurrentProgress
+        {
+            get => currentProgress;
+            set
+            {
+                Set(nameof(CurrentProgress), ref currentProgress, value);
+            }
+        }
+
+        public RelayCommand ViewLogCommand { get; set; }
+        public RelayCommand RandomizeSettingsCommand { get; set; }
+        public RelayCommand GenerateCommand { get; set; }
 
     }
 }
