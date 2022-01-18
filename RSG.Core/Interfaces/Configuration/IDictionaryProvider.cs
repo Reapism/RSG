8d2677fb20d8efe2ddcb88a254d3e4eb7361d173
Anthony J <reapsprgm@gmail.com>
2022-01-18 00:29:33 -0500

    Remove 1to1 configuration interfaces as they are not necessary anymore.
    - Generators will except a TRequest and return a TResult of the corresponding generate type.
    - - i.e. IStringRequest, IStringResult, IDictionaryRequest, IDicitonaryResult

---
+++ RSG.Core/Interfaces/Configuration/IDictionaryProvider.cs
@@ -0,0 +1,13 @@
+﻿using RSG.Core.Models;
+using System.Collections.Generic;
+
+namespace RSG.Core.Interfaces.Configuration
+{
+    public interface IDictionaryProvider
+    {
+        /// <summary>
+        /// Gets or sets a <see cref="IList{T}"/> of <see cref="RsgDictionary"/>(s).
+        /// </summary>
+        IList<RsgDictionary> Dictionaries { get; set; }
+    }
+}
