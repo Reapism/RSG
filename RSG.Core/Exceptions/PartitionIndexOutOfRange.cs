275500b4e4d8e11122b132790dae0b9592945655
Anthony J <reapsprgm@gmail.com>
2022-01-18 23:45:30 -0500

    Add RsgException and PartitionIndexOutOfRange

---
+++ RSG.Core/Exceptions/PartitionIndexOutOfRange.cs
@@ -0,0 +1,10 @@
+﻿namespace RSG.Core.Exceptions
+{
+    public class PartitionIndexOutOfRange : RsgException
+    {
+        public PartitionIndexOutOfRange(int input, string parameterName)
+            : base($"The parameter '{parameterName}' value {input} is out of range for the partition.")
+        {
+        }
+    }
+}
