85e29a757b545cbf617f9fd043dc5588b3a3e4be
Anthony J <reapsprgm@gmail.com>
2022-02-05 02:57:06 -0500

    Remove WebClient usage and replace with HttpClient for now
    - Still need to convert this to a scoped service instead of static, and require httpclientfactory as a dependency

--- RSG.Core/Utilities/DownloadUtility.cs
+++ RSG.Core/Utilities/DownloadUtility.cs
@@ -1,4 +1,6 @@
 ﻿using System.Net;
+using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace RSG.Core.Utilities
@@ -16,13 +18,15 @@ namespace RSG.Core.Utilities
         /// <returns>The resource as a <see langword="string"/>.</returns>
         /// <exception cref="WebException">Thrown when something exceptional 
         /// happens when downloading the file from the url.</exception>
-        public static async Task<string> DownloadFileAsString(string directUrl)
+        public static async Task<string> DownloadFileAsStringAsync(string directUrl, CancellationToken cancellationToken)
         {
             if (string.IsNullOrEmpty(directUrl))
+            {
                 return string.Empty;
+            }
 
-            using var webClient = new WebClient();
-            var reply = await webClient.DownloadStringTaskAsync(directUrl);
+            using var client = new HttpClient();
+            var reply = await client.GetStringAsync(directUrl, cancellationToken);
 
             return reply;
         }
