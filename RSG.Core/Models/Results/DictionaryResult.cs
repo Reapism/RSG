770f034a1113f8ade41104b0e439f36043308189
Anthony J <reapsprgm@gmail.com>
2022-02-05 03:30:15 -0500

    Improvements to the Result infrastructure
    - Results are now generic and require a TRequest : IRequest

---
+++ RSG.Core/Models/Results/DictionaryResult.cs
@@ -0,0 +1,31 @@
+﻿using RSG.Core.Factories;
+using RSG.Core.Interfaces;
+using RSG.Core.Interfaces.Request;
+using RSG.Core.Interfaces.Result;
+using System;
+
+namespace RSG.Core.Models.Results
+{
+    /// <summary>
+    /// Represents properties of an RSG dictionary.
+    /// </summary>
+    public class DictionaryResult : Result<IDictionaryRequest>, IDictionaryResult
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DictionaryResult"/> class.
+        /// </summary>
+        /// <param name="request">The request used to generate this result.</param>
+        /// <param name="duration">The duration of generating this result.</param>
+        public DictionaryResult(IDictionaryRequest request, TimeSpan duration, WordContainer wordContainer)
+            : base(request, duration)
+        {
+            Words = wordContainer;
+        }
+
+        /// <inheritdoc/>
+        public IDictionaryRequest Request { get; }
+
+        /// <inheritdoc/>
+        public WordContainer Words { get; set; }
+    }
+}
---
+++ RSG.Core/Models/Results/Result.cs
@@ -0,0 +1,29 @@
+﻿using RSG.Core.Interfaces.Request;
+using RSG.Core.Interfaces.Requests;
+using RSG.Core.Interfaces.Result;
+using System;
+
+namespace RSG.Core.Models.Results
+{
+    /// <summary>
+    /// A class containing a result.
+    /// </summary>
+    /// <typeparam name="TRequest">A request instance.</typeparam>
+    public class Result<TRequest> : IResult
+        where TRequest : class, IRequest
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Result"/> class.
+        /// </summary>
+        /// <param name="request">A request instance used to create a result.</param>
+        /// <param name="duration">The duration of the generated result.</param>
+        public Result(TRequest request, TimeSpan duration)
+        {
+            Request = Request;
+            Duration = duration;
+        }
+
+        public TRequest Request { get; }
+        public TimeSpan Duration { get; }
+    }
+}
