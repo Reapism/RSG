8f08db8d1cc2de5e4089be34a883921f86dcd78c
Anthony J <reapsprgm@gmail.com>
2022-02-09 00:56:37 -0500

    Introduce FrequencyUnit which helps determine the iterations per unit for something
    - made the statistics modular, can compute any unit by supplying the class with the following function to compute it given the iterations per second.

--- RSG.Core/Interfaces/IIterationsFrequency.cs
+++ RSG.Core/Interfaces/IIterationsFrequency.cs
@@ -1,21 +1,60 @@
-﻿using System;
+﻿using RSG.Core.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
 
 namespace RSG.Core.Interfaces
 {
     public interface IIterationsFrequency
     {
-        TimeSpan Duration { get; set; }
+        IEnumerable<FrequencyUnit> IterationFrequencies { get; }
+    }
 
-        TimeSpan IterationsPerSecond { get; set; }
+    /// <summary>
+    /// Embeds information on computing the number of iterations it would take
+    /// to reach an single unit of something.
+    /// </summary>
+    public class FrequencyUnit
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrequencyUnit"/> class.
+        /// </summary>
+        /// <param name="unit">The unit to compute to.</param>
+        /// <param name="iterationsPerSecond">The number of iterations something completes in.</param>
+        /// <param name="iterationsPerUnitFunc">A function to return the number of iterations it would take to reach <paramref name="unit"/></param>
+        public FrequencyUnit(string unit, BigInteger iterationsPerSecond, Func<BigInteger> iterationsPerUnitFunc)
+        {
+            Unit = unit;
+            IterationsPerSecond = iterationsPerSecond;
+            ComputeIterationsPerUnit = iterationsPerUnitFunc ?? throw new ArgumentNullException(nameof(iterationsPerUnitFunc));
+        }
 
-        TimeSpan IterationsPerMinute { get; set; }
+        public string Unit { get; }
 
-        TimeSpan IterationsPerHour { get; set; }
+        public BigInteger IterationsPerSecond { get; }
 
-        TimeSpan IterationsPerDay { get; set; }
+        public Func<BigInteger> ComputeIterationsPerUnit { get; }
 
-        TimeSpan IterationsPerYear { get; set; }
+        public override string ToString()
+        {
+            return $"You will iterate {ComputeIterationsPerUnit.Invoke():n0} times per {Unit}.";
+        }
+    }
 
-        TimeSpan IterationsPerCentury { get; set; }
+    public class Test
+    {
+        private readonly BigInteger iterationsPerSecond;
+
+        public Test(BigInteger iterationsPerSecond)
+        {
+            var freqUnit = new FrequencyUnit("Day", iterationsPerSecond, GetIterationsPerDay);
+            this.iterationsPerSecond = iterationsPerSecond;
+            freqUnit.ToString();
+        }
+
+        private BigInteger GetIterationsPerDay()
+        {
+            return iterationsPerSecond * 60 * 60 * 24;
+        }
     }
 }
